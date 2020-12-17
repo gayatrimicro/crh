f50245d75b5596c04166c04466456bce27f3e0c4
prashantgmicro <pgole31@gmail.com>
2020-12-17 17:55:14 +0530

    changes

---
+++ copy-of-services/index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="shortcut icon" href="https://www.bharatserums.com/assets/images/favi.png" />
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  	<link rel="stylesheet" href="../assets/css/style.css">
+  	<link rel="stylesheet" href="../assets/css/media.css">
+  	<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+HK:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+</head>
+<body>
+	<section class="Header_part">
+		<div class="topMenu">
+			<div class="shpmen"></div>
+			<div class="row">
+				<div class="col">
+					<a href="">Covid-19</a> &nbsp;&nbsp;|&nbsp;&nbsp; <a href="">Request an Appointment</a>
+				</div>
+				<div class="col">
+					<button><i class="fa fa-sign-in" aria-hidden="true"></i>&nbsp; Log In</button>
+				</div>
+			</div>					
+		</div>
+		<div class="box">
+			<div class="row">
+			<div class="col pad_0"><img src="../assets/img/logo.webp" alt="logo"></div>
+				<div class="col pad_0">					
+					<ul>
+						<li><a href="">Home</a></li>|
+						<li class="PosRel">
+							<a href="">Services</a>
+							<div class="SubMenu">
+								<ul>
+									<li><a href="">Emergency Care</a></li>
+									<li><a href="">Surgical Specialities</a></li>
+									<li><a href="">Speciality Services</a></li>
+									<li><a href="">Hospital Services</a></li>
+								</ul>
+							</div>
+						</li>|
+						<li><a href="">Why CRH</a></li>|
+						<li><a href="">Patient Visits</a></li>|
+						<li><a href="">Patient Pre-registration</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>		
+	</section>
+	<section class="PosRel">
+		<div class="img_box">
+			<img src="../assets/img/banslid1.webp" alt="img">
+		</div>
+		<div class="bannertxt">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<p>PRE-REGISTRATION</p>
+						<h2>Your health, your journey.</h2>
+						<p>Your online healthcare platform.</p>
+					</div>
+					<div class="col text-right">
+						<button class="btn_site">Sign Up</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="pad75">
+		<div class="container">
+			<h3 class="sech3">Simplifying your healthcare online experience.</h3>
+			<div class="BoxCon bx eff">
+				<p>Pre-registering for your appointment shortens the check-in process at the facility.  It only takes 10 to 15 minutes to complete. Follow the guidelines below to expedite the pre-registration and appointment experience.</p>
+				<p>Please note if you do not initiate the pre-registration process with us, a representative from our Pre-Registration Department may call you to obtain the necessary information for the registration process.</p>
+				<p><b>Items to Have on Hand to Complete Pre-Registration</b></p>
+				<ul>
+					<li>Insurance card</li>
+					<li>Driver’s license</li>
+					<li>Emergency contact information – name, address and phone number of someone who can be contacted in the event of an emergency.</li>
+				</ul>
+				<p>All information is secure and confidential.</p>
+				<button class="btn_site">Sign Up</button>
+			</div>
+		</div>
+	</section>
+	<section class="footersec">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<img src="../assets/img/logo.webp" class="flogo" alt="logo">
+					<p>2600 W. Pleasant Run Rd.<br>Lancaster, TX 75146</p>
+					<p>Ph. <b><a href="tel:(469) 297-5321">(469) 297-5321</a></b></p>
+					<p>Monday - Sunday |  Open 24/7</p>
+				</div>
+				<div class="col">
+					<h4 class="mart20">Receiving Care</h4>
+					<ul>
+						<li><a href="">Primary Care</a></li>
+						<li><a href="">Surgical Specialties</a></li>
+						<li><a href="">Specialty Services</a></li>
+						<li><a href="">Hospital Services</a></li>
+						<li><a href="">Emergency Services</a></li>
+						<li><a href="">All Services</a></li>
+					</ul>
+				</div>
+				<div class="col">
+					<h4 class="mart20">About Us</h4>
+					<ul>
+						<li><a href="">Location</a></li>
+						<li><a href="">Contact Us</a></li>
+						<li><a href="">About Us</a></li>
+					</ul>
+				</div>
+				<div class="col">
+					<h4 class="mart20">Careers</h4>
+					<ul>
+						<li><a href="">Join Our Team</a></li>
+					</ul>
+					<input type="text" class="form-control" placeholder="Search">
+				</div>
+			</div>
+			<hr>
+			<p class="cop">© 2020 Crescent Regional Hospital</p>
+		</div>		
+	</section>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>	
+</body>
+</html>
