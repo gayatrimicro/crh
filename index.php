780f6149729f45337bf1fb7047ea1233275d4892
prashantgmicro <pgole31@gmail.com>
2020-12-17 17:33:05 +0530

    changes

---
+++ index.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="shortcut icon" href="https://www.bharatserums.com/assets/images/favi.png" />
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  	<link rel="stylesheet" href="assets/css/style.css">
+  	<link rel="stylesheet" href="assets/css/media.css">
+  	<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+HK:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+</head>
+<body>
+	<section class="Header_part">
+		<div class="topMenu">
+			<div class="shpmen"></div>
+			<div class="row">
+				<div class="col">
+					<a href="">Covid-19</a> &nbsp;&nbsp;|&nbsp;&nbsp; <a href="">Request an Appointment</a>
+				</div>
+				<div class="col">
+					<button><i class="fa fa-sign-in" aria-hidden="true"></i>&nbsp; Log In</button>
+				</div>
+			</div>					
+		</div>
+		<div class="box">
+			<div class="row">
+			<div class="col pad_0"><img src="assets/img/logo.webp" alt="logo"></div>
+				<div class="col pad_0">					
+					<ul>
+						<li><a href="">Home</a></li>
+						<li class="PosRel">
+							<a href="">Services</a>
+							<div class="SubMenu">
+								<ul>
+									<li><a href="">Emergency Care</a></li>
+									<li><a href="">Surgical Specialities</a></li>
+									<li><a href="">Speciality Services</a></li>
+									<li><a href="">Hospital Services</a></li>
+								</ul>
+							</div>
+						</li>
+						<li><a href="">Why CRH</a></li>
+						<li><a href="">Patient Visits</a></li>
+						<li><a href="">Patient Pre-registration</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>		
+	</section>
+	<section class="Video_banner">
+		<video loop id="Vid" autoplay muted="muted">
+  			<source src="assets/video/file.mp4" type="video/mp4">
+		</video>
+		<div class="VidBannr">
+			<div class="SecondBx"></div>
+			<p class="mart50">QUALITY CARE</p>
+			<h2>Now more <br> than ever, better <br>care matters.</h2>
+			<p>In our hospital, we treat patients like family. That is why we have providers known as Hospitalists to help take care of you.</p>
+			<button class="btn_site">Browse Services</button>
+		</div>
+	</section>
+	<section class="TabBtn">
+		<div class="container">
+			<div class="row">
+				<div class="col"><a href="">Emergency Services</a></div>
+				<div class="col"><a href="">Primary Care</a></div>
+				<div class="col"><a href="">Surgical Specialties</a></div>
+				<div class="col"><a href="">Hospital Services</a></div>
+			</div>
+		</div>						
+	</section>
+	<section class="pad75">
+		<div class="container">
+			<h3 class="sech3">A special note to our patients</h3>
+			<a href="">Find out more &nbsp;&nbsp;<i class="fa fa-caret-right" aria-hidden="true"></i></a>
+			<div class="BoxCon bx eff">
+				<p>Crescent Regional Hospital (CRH) is closely monitoring the coronavirus outbreak, known as COVID-19. We regularly treat patients with infectious diseases, and CMCL physicians, nurses, and clinical staff are highly trained in infection prevention protocols.</p>
+				<p>Specific to COVID-19, we are following the procedures outlined by the CDC and state and federal healthcare authorities.</p>
+				<p>Some of the steps we have taken include:</p>
+				<ul>
+					<li>Prescreening and testing for patients and staff</li>
+					<li>Social distancing and face covering</li>
+					<li>Separate COVID-19 care facilities with dedicated staffing</li>
+					<li>Visitor restrictions</li>
+					<li>Rigorous infection prevention and cleaning</li>
+				</ul>
+				<p>As always, if you are experiencing a medical emergency, call 911. If available, put on a face mask before emergency medical services arrive.</p>
+			</div>
+		</div>
+	</section>
+	<section class="PosRel">
+		<div class="img_box">
+			<img src="assets/img/ban1.webp" alt="img">
+		</div>
+		<div class="bannertxt">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<p>TACKLING COMPLEX CASES</p>
+						<h2>Our doctors<br> specialize in you.</h2>
+						<p>We tackle the most difficult challenges to provide the best possible health outcome.</p>
+					</div>
+					<div class="col text-right">
+						<button class="btn_site">Browse Services</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="Sec3">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="BoxCon bx eff haut">
+						<h3 class="sech3">A special note to our patients</h3>
+						<a href="#">Find out more &nbsp;&nbsp;<i class="fa fa-caret-right" aria-hidden="true"></i></a>
+						<ul class="mart20">
+							<li>BARIATRIC</li>
+							<li>GASTROENTEROLOGY</li>
+							<li>GENERAL SURGERY</li>
+							<li>GYNECOLOGY</li>
+							<li>ORTHOPEDICS</li>
+							<li>PAIN MANAGEMENT</li>
+							<li>PODIATRY</li>
+							<li>SPINAL HEALTH</li>
+							<li>UROLOGY</li>
+							<li>VASCULAR SURGERYÂ </li>
+						</ul>
+					</div>				
+				</div>
+				<div class="col">
+					<div class="BoxCon bx eff haut">
+						<h3 class="sech3">Specialty services</h3>
+						<a href="#">Find out more &nbsp;&nbsp;<i class="fa fa-caret-right" aria-hidden="true"></i></a>
+						<ul class="mart20">
+							<li>BREAKTHRU PROGRAM</li>
+							<li>PHYSICAL THERAPY</li>
+							<li>SPECIALTY CLINIC</li>
+							<li>WOUND CARE</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col">
+					<div class="BoxCon bx eff haut">
+						<h3 class="sech3">Hospital services</h3>
+						<a href="#">Find out more &nbsp;&nbsp;<i class="fa fa-caret-right" aria-hidden="true"></i></a>
+						<ul class="mart20">
+							<li>EMERGENCY DEPARTMENT</li>
+							<li>INPATIENT SERVICES</li>
+							<li>RADIOLOGY</li>
+							<li>AUTO ACCIDENTS </li>
+							<li>WORKER 'S COMPENSATION</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="PosRel bacclr">
+		<img src="assets/img/cmcl_family.webp" class="img_pos" alt="img">
+		<div class="bannertxt">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<p>A TRUE COMMUNITY HOSPITAL</p>
+						<h2>Accessible healthcare<br> for everyone.</h2>
+						<p>Providing care for the residents of Lancaster and all surrounding areas.</p>
+					</div>
+					<div class="col text-right">
+						<button class="btn_site">Browse Services</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="pad75">
+		<div class="container">
+			<h3 class="sech3">Personalized care in your community</h3>
+			<a href="">Find out more &nbsp;&nbsp;<i class="fa fa-caret-right" aria-hidden="true"></i></a>
+			<div class="BoxCon bx eff">
+				<p>Located in Lancaster, Texas, a southern suburb of Dallas, Crescent Regional Hospital opened to the community in 2013.</p>
+				<p>Our goal is to provide care for the residents of Lancaster and all surrounding areas and to become a vital part of the community. Being a smaller hospital allows us the ability to provide very personalized care utilizing a team approach. It allows us the ability to be more responsive and innovative throughout your care.</p>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13444.473223778561!2d-96.812489!3d32.6030305!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa6cfc014326fa8b8!2sCrescent%20Regional%20Hospital!5e0!3m2!1sen!2sin!4v1608194613915!5m2!1sen!2sin" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>				
+			</div>
+		</div>
+	</section>
+	<section class="Banner2">
+		<span class="PosRel">
+			<img src="assets/img/Banner_FastER.webp" class="img_docban" alt="Banner_FastER">
+			<img src="assets/img/Web_FastER_2.webp" class="img_doctor" alt="Web_FastER_2">
+		</span>		
+	</section>
+	<section class="footersec">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<img src="assets/img/logo.webp" class="flogo" alt="logo">
+					<p>2600 W. Pleasant Run Rd.<br>Lancaster, TX 75146</p>
+					<p>Ph. <b><a href="tel:(469) 297-5321">(469) 297-5321</a></b></p>
+					<p>Monday - Sunday |  Open 24/7</p>
+				</div>
+				<div class="col">
+					<h4 class="mart20">Receiving Care</h4>
+					<ul>
+						<li><a href="">Primary Care</a></li>
+						<li><a href="">Surgical Specialties</a></li>
+						<li><a href="">Specialty Services</a></li>
+						<li><a href="">Hospital Services</a></li>
+						<li><a href="">Emergency Services</a></li>
+						<li><a href="">All Services</a></li>
+					</ul>
+				</div>
+				<div class="col">
+					<h4 class="mart20">About Us</h4>
+					<ul>
+						<li><a href="">Location</a></li>
+						<li><a href="">Contact Us</a></li>
+						<li><a href="">About Us</a></li>
+					</ul>
+				</div>
+				<div class="col">
+					<h4 class="mart20">Careers</h4>
+					<ul>
+						<li><a href="">Join Our Team</a></li>
+					</ul>
+					<input type="text" class="form-control" placeholder="Search">
+				</div>
+			</div>
+			<hr>
+			<p class="cop">© 2020 Crescent Regional Hospital</p>
+		</div>		
+	</section>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>	
+</body>
+</html>
